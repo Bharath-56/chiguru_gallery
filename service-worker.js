1557613a4477f0d3e5babd08f4cf6599d71df3ae
Bharath S M <bharathmanu1235@gmail.com>
2025-10-06 17:47:49 +0530

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'chiguru-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
