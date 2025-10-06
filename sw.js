c04efbdda5bede47e29120bee9fc8678187b25c5
Bharath S M <bharathmanu1235@gmail.com>
2025-10-06 19:32:55 +0530

    Initial commit

--- sw.js
+++ sw.js
@@ -2,12 +2,11 @@ self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("chiguru-cache").then((cache) => {
       return cache.addAll([
-        "/chiguru_website/",
-        "/chiguru_website/index.html",
-        "/chiguru_website/css/style.css",
-        "/chiguru_website/js/script.js",
-        "/chiguru_website/icons/icon-192x192.png",
-        "/chiguru_website/icons/icon-512x512.png"
+        "./",
+        "./index.html",
+        "./style.css",
+        "./icons/icon-192x192.png",
+        "./icons/icon-512x512.png"
       ]);
     })
   );
